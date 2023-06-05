9e7d023ec30c7a3df48f5a12ce167f6d6b3c8a12
aaryanmehta26 <aaryanmehta2699@gmail.com>
2023-06-06 00:51:23 +0530

    added google maps using typescript and following design principles

---
+++ src/User.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    constructor (){
+        this.name = faker.internet.userName();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        }
+    }
+
+    markerContent(): string {
+        return `User name is ${this.name}`
+    }
+}
